4aa66c22eb5e7e2aa6aeefefa117ce208dcbe194
sakibbdcallingit <mahabuburbdcalling@gmail.com>
2025-07-16 18:37:36 +0600

    add

--- src/app/modules/auth/auth.validation.ts
+++
@@ -1,7 +0,0 @@
-// auth.validation.ts
-import { z } from 'zod';
-
-export const loginValidationSchema = z.object({
-  id: z.string({ required_error: 'User ID is required' }),
-  password: z.string({ required_error: 'Password is required' }),
-});
